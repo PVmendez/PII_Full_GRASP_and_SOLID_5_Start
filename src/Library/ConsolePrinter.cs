b32e01ae75a5c5d583fe28759f63f4288552f7bb
Pablo Méndez <valentino.mendez@correo.ucu.edu.uy>
2021-10-20 10:26:52 -0300

    Solución

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,15 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
+        public void PrintRecipe(IRecipe recipe)
+        {
+            Console.WriteLine(recipe.GetTextToPrint());
+        }
+
         public void PrintRecipe(Recipe recipe)
         {
             Console.WriteLine(recipe.GetTextToPrint());
         }
+
     }
 }
